f504f250c3fa2a0a5c98f1538098e1205cdf75f9
Cekavis <cmxrynp@gmail.com>
2026-06-19 23:02:40 +0800

    Fix Package workflow build environment

--- .github/actions/build-for-linux/entrypoint.sh
+++ .github/actions/build-for-linux/entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-wget https://nodejs.org/dist/v19.8.1/node-v19.8.1-linux-x64.tar.xz
-tar -Jxvf ./node-v19.8.1-linux-x64.tar.xz
-export PATH=$(pwd)/node-v19.8.1-linux-x64/bin:$PATH
-npm install pnpm -g
+NODE_VERSION=v21.7.3
+PNPM_VERSION=9
+
+wget "https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-linux-x64.tar.xz"
+tar -Jxvf "./node-${NODE_VERSION}-linux-x64.tar.xz"
+export PATH="$(pwd)/node-${NODE_VERSION}-linux-x64/bin:$PATH"
+npm install "pnpm@${PNPM_VERSION}" -g
 
 rustup target add "$INPUT_TARGET"
 rustup toolchain install --force-non-host "$INPUT_TOOLCHAIN"
